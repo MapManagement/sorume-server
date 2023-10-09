609da39f596b6dd883b87b93aa7604998dd5f7df
Jan <MapManagement@protonmail.com>
2023-10-09 21:53:43 +0200

    Add logging calls to group chat member operations

--- src/database/src/group_chat_member_operations.rs
+++ src/database/src/group_chat_member_operations.rs
@@ -1,5 +1,6 @@
 use crate::{check_group_chat_exists, check_profile_exists};
 use entities::*;
+use log::*;
 use sea_orm::*;
 
 pub async fn insert_group_chat_member(
@@ -10,12 +11,22 @@ pub async fn insert_group_chat_member(
     let target_profile = check_profile_exists(profile_id, connection).await;
 
     if target_profile.is_err() {
+        warn!(
+            "C: Unable to add a profile, which does not exist, to a group: {}",
+            target_profile.as_ref().unwrap_err()
+        );
+
         return Err(target_profile.unwrap_err());
     }
 
     let target_group_chat = check_group_chat_exists(group_chat_id, connection).await;
 
     if target_group_chat.is_err() {
+        warn!(
+            "C: Unable to add a profile to a group chat, which does not exist: {}",
+            target_group_chat.as_ref().unwrap_err()
+        );
+
         return Err(target_group_chat.unwrap_err());
     }
 
@@ -27,6 +38,11 @@ pub async fn insert_group_chat_member(
     .insert(connection)
     .await;
 
+    info!(
+        "C: The member '{:?}' has been added to group chat '{:?}'",
+        profile_id, group_chat_id
+    );
+
     return new_member;
 }
 
@@ -37,14 +53,26 @@ pub async fn get_members_of_group(
     let target_group_chat = check_group_chat_exists(group_chat_id, connection).await;
 
     if target_group_chat.is_err() {
+        debug!("R: Group chat with ID {:?} does not exist", group_chat_id);
         return Err(target_group_chat.unwrap_err());
     }
 
-    Ok(target_group_chat
+    let members_result = target_group_chat
         .unwrap()
         .find_related(group_chat_member::Entity)
         .all(connection)
-        .await?)
+        .await;
+
+    match members_result {
+        Ok(members) => {
+            debug!("R: Members of group chat have been found");
+            return Ok(members);
+        }
+        Err(err) => {
+            debug!("R: Group chat with ID {:?} does not exist", group_chat_id);
+            return Err(err);
+        }
+    }
 }
 
 pub async fn get_memberships_of_profile(
@@ -54,29 +82,38 @@ pub async fn get_memberships_of_profile(
     let target_profile = check_profile_exists(profile_id, connection).await;
 
     if target_profile.is_err() {
+        debug!("R: Profile with ID {:?} does not exist", profile_id);
         return Err(target_profile.unwrap_err());
     }
 
-    let memberships = target_profile
+    let memberships_result = target_profile
         .unwrap()
         .find_related(group_chat_member::Entity)
         .all(connection)
-        .await?;
+        .await;
 
     let mut groups = Vec::new();
 
-    for membership in memberships {
-        let group = membership
-            .find_related(group_chat::Entity)
-            .one(connection)
-            .await?;
+    match memberships_result {
+        Ok(memberships) => {
+            for membership in memberships {
+                let group = membership
+                    .find_related(group_chat::Entity)
+                    .one(connection)
+                    .await?;
 
-        if group.is_none() {
-            continue;
+                if group.is_none() {
+                    continue;
+                }
+
+                groups.push(group.unwrap());
+            }
         }
-
-        groups.push(group.unwrap());
-    }
+        Err(err) => {
+            debug!("R: Profile with ID {:?} does not exist", profile_id);
+            return Err(err);
+        }
+    };
 
     Ok(groups)
 }
@@ -93,12 +130,36 @@ pub async fn delete_single_membership(
         .await?;
 
     if target_membership.is_none() {
+        warn!(
+            "D: A memberhsip of profile with ID {:?} in group chat with ID {:?} does not exist",
+            profile_id, group_chat_id
+        );
+
         return Err(DbErr::Custom(
             "Couldn't find a group chat with the specified identifier.".to_owned(),
         ));
     }
 
-    Ok(target_membership.unwrap().delete(connection).await?)
+    let delete_result = target_membership.unwrap().delete(connection).await;
+
+    match delete_result {
+        Ok(deleted_membership) => {
+            debug!(
+                "D: Membership has been deleted: Profile {}, Group chat {}",
+                profile_id, group_chat_id
+            );
+
+            return Ok(deleted_membership);
+        }
+        Err(err) => {
+            warn!(
+                "D: Couldn't delete the membership: Profile: {}, Group chat {}",
+                profile_id, group_chat_id
+            );
+
+            return Err(err);
+        }
+    }
 }
 
 pub async fn delete_members_of_group(
@@ -108,13 +169,29 @@ pub async fn delete_members_of_group(
     let target_group_chat = check_group_chat_exists(group_chat_id, connection).await;
 
     if target_group_chat.is_err() {
+        warn!("D: Group chat with ID {} does not exist", group_chat_id);
         return Err(target_group_chat.unwrap_err());
     }
 
-    Ok(group_chat_member::Entity::delete_many()
+    let delete_result = group_chat_member::Entity::delete_many()
         .filter(group_chat_member::Column::GroupChatId.eq(group_chat_id))
         .exec(connection)
-        .await?)
+        .await;
+
+    match delete_result {
+        Ok(deleted_memberships) => {
+            debug!(
+                "D: Memberships of group chat {} have been deleted",
+                group_chat_id
+            );
+
+            return Ok(deleted_memberships);
+        }
+        Err(err) => {
+            warn!("D: Group chat with ID {} does not exist", group_chat_id);
+            return Err(err);
+        }
+    }
 }
 
 pub async fn delete_memberships_of_profile(
@@ -124,11 +201,23 @@ pub async fn delete_memberships_of_profile(
     let target_profile = check_profile_exists(profile_id, connection).await;
 
     if target_profile.is_err() {
+        warn!("D: Profile with ID {:?} does not exist", profile_id);
         return Err(target_profile.unwrap_err());
     }
 
-    Ok(group_chat_member::Entity::delete_many()
+    let delete_result = group_chat_member::Entity::delete_many()
         .filter(group_chat_member::Column::ProfileId.eq(profile_id))
         .exec(connection)
-        .await?)
+        .await;
+
+    match delete_result {
+        Ok(deleted_memberships) => {
+            debug!("D: Memberships of profile {} have been deleted", profile_id);
+            return Ok(deleted_memberships);
+        }
+        Err(err) => {
+            warn!("D: Profile with ID {} does not exist", profile_id);
+            return Err(err);
+        }
+    }
 }
